f3fe9fd10bc0dd91802cd37b1197a9f88bd293a3
ksdfg <ksdfg123@gmail.com>
2025-12-09 10:36:48 +0530

    feat(health): add health check
    Signed-off-by: ksdfg <ksdfg123@gmail.com>

--- app/main.py
+++ app/main.py
@@ -1,6 +1,9 @@
 from fastapi import FastAPI
 
+from app.api.v1.api import router
+
 app = FastAPI(
     title="Bill Splitter",
     description="A small utility to split bills amongst friends",
 )
+app.include_router(router, prefix="/api/v1")

b5f99791ee0430fc35e766f72c4e7fcb5a1655fa
ksdfg <ksdfg123@gmail.com>
2025-12-14 00:53:58 +0530

    feat(backend): add setting to allow hosts for cors

--- backend/app/core/settings.py
+++ backend/app/core/settings.py
@@ -2,6 +2,8 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
 class Settings(BaseSettings):
+    CORS_ALLOW_HOSTS: list[str] = []
+
     GEMINI_API_KEY: str | None = None
 
     model_config = SettingsConfigDict(env_file=".env", env_file_encoding="utf-8")
--- backend/app/main.py
+++ backend/app/main.py
@@ -1,9 +1,20 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from app.api.v1.api import router
+from app.core.settings import settings
 
 app = FastAPI(
     title="Bill Splitter",
     description="A small utility to split bills amongst friends",
 )
+
+app.add_middleware(
+    CORSMiddleware,  # type: ignore
+    allow_origins=settings.CORS_ALLOW_HOSTS,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 app.include_router(router, prefix="/api/v1")
